3a110a4923f2f93531c043571470dd38e032b129
Luis Velasco <luis.velasco.c@outlook.com>
2021-09-07 23:45:02 -0400

    complete exercise 18.6

--- src/kindle/exercises/RecursionChapter.java
+++ src/kindle/exercises/RecursionChapter.java
@@ -6,18 +6,33 @@ public class RecursionChapter {
         int base = 3;
         int power = 10;
         System.out.println(base + " to the " + power + " power: " + powerOf(12, base));
+        System.out.println("Sum of : " + nSum(21));
+    }
+
+
+    /**
+     * 18.6
+     * n = 5
+     * nSum(5)--> returns 5 + 10 = 15
+     *      nSum(4)--> returns 4 + 6 = 10
+     *           nSum(3)--> returns 3 + 3 = 6
+     *                 nSum(2)--> returns 2 + 1 = 3
+     *                      nSum(1)--> returns 1
+     */
+    public static int nSum(int n){
+        if(n == 1) return 1;
+        return n + nSum(n - 1);
     }
 
     /**
-     *
      * 18.4 and 18.5 exercises
      *
      * n = 3
      *
-     * method(3) -->  returns 2 * 4 = 8
-     *          method(2)--> returns 2 * 2 = 4
-     *                  method(1)-->   returns 2 * 1 = 2
-     *                          method(0)-->  returns 1
+     * method(3)-->  returns 2 * 4 = 8
+     *        method(2)--> returns 2 * 2 = 4
+     *                 method(1)-->   returns 2 * 1 = 2
+     *                         method(0)-->  returns 1
      */
     public static int powerOf(int n, int base){
         if(n == 0) return 1;
