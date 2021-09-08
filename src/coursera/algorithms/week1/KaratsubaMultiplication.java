a6b4e70c30f2a9ada3167df5ac73de331ea1ab86
Luis Velasco <luis.velasco.c@outlook.com>
2021-09-07 23:45:54 -0400

    Initial code for Karatsuba multiplication

--- src/coursera/algorithms/week1/KaratsubaMultiplication.java
+++ src/coursera/algorithms/week1/KaratsubaMultiplication.java
@@ -1,2 +1,18 @@
-package coursera.algorithms.week1;public class KaratsubaMultiplication {
+package coursera.algorithms.week1;
+
+public class KaratsubaMultiplication {
+
+    public static void main(String[] args){
+        String x = "123";
+        String y = "204";
+
+    }
+
+    public static String multiply(String a, String b){
+        if(a.length() == 1 && b.length() == 1)
+            return "" + Integer.parseInt(a) * Integer.parseInt(b);
+
+        return multiply(a.substring(0, a.length() / 2), b.substring(0, b.length() / 2));
+    }
+
 }
