b2405a32b15e24ae088f9492382a399183b292f2
Luis Velasco <luis.velasco.c@outlook.com>
2021-09-16 23:02:10 -0400

    Add functionality to pad the smaller number (digit wise) with leading 0s so that the recursive calls don't fail.

--- src/coursera/algorithms/week1/KaratsubaMultiplication.java
+++ src/coursera/algorithms/week1/KaratsubaMultiplication.java
@@ -3,16 +3,57 @@ package coursera.algorithms.week1;
 public class KaratsubaMultiplication {
 
     public static void main(String[] args){
-        String x = "123";
-        String y = "204";
 
+        long x = 245524552455L;
+        long y = 245524552455L;
+        System.out.println("Java's Multiplication product: "  + (x * y));
+        System.out.println("My multiplication product: " + multiply(x, y));
     }
 
-    public static String multiply(String a, String b){
-        if(a.length() == 1 && b.length() == 1)
-            return "" + Integer.parseInt(a) * Integer.parseInt(b);
 
-        return multiply(a.substring(0, a.length() / 2), b.substring(0, b.length() / 2));
+    /**
+     *
+     * @param x
+     * @param y
+     * @return
+     */
+    public static long multiply(long x, long y){
+        String xStr = x + "";
+        String yStr = y + "";
+        int n = Math.max(xStr.length(), yStr.length());
+
+        if(xStr.length() < yStr.length()){ // making sure the smaller digit number gets padded with preceding 0s
+            xStr = String.format("%0" + n + "d", x);
+        }
+        if(yStr.length() < xStr.length()){
+            yStr = String.format("%0" + n + "d", y); // making sure the smaller digit number gets padded with preceding 0s
+        }
+
+        if(n == 1){
+            return x * y;
+        }
+
+        int middleX = getMiddleIndex(n);
+        long a = Long.parseLong(xStr.substring(0, middleX)); // get first half of x
+        long b = Long.parseLong(xStr.substring(middleX)); // get second half of x
+        long c = Long.parseLong(yStr.substring(0, middleX)); // get the first half of y
+        long d = Long.parseLong(yStr.substring(middleX)); // get the second half of y
+
+        long acResult = multiply(a, c);
+        long bcResult = multiply(b, d);
+        long aPlusBTimesCPlusD = multiply(a + b, c + d);
+        long adPlusBcByGaussTrick = (aPlusBTimesCPlusD - acResult) - bcResult;
+
+        return (long)(Math.pow(10, n) * acResult + Math.pow(10, n / 2) * adPlusBcByGaussTrick + bcResult);
+    }
+
+    /**
+     * Gets the middle index if n is even, splits the digits in half + 1 for the first half.
+     * @param n the number of digits
+     * @return int
+     */
+    private static int getMiddleIndex(int n){
+        return n % 2 == 0 ? n / 2 : (n / 2) + 1;
     }
 
 }
