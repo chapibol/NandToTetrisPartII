260425fa0a292d71dfc7f564e8f584a16ca2e05f
Luis Velasco <luis.velasco.c@outlook.com>
2021-09-27 16:53:39 -0400

    add programming assignement case

--- src/coursera/algorithms/week1/KaratsubaMultiplication.java
+++ src/coursera/algorithms/week1/KaratsubaMultiplication.java
@@ -6,8 +6,8 @@ public class KaratsubaMultiplication {
 
     public static void main(String[] args){
 
-        BigInteger x = new BigInteger("134");
-        BigInteger y = new BigInteger("4");
+        BigInteger x = new BigInteger("3141592653589793238462643383279502884197169399375105820974944592");
+        BigInteger y = new BigInteger("2718281828459045235360287471352662497757247093699959574966967627");
 
         System.out.println("Java's BigInteger Multiplication product: "  + x.multiply(y));
 
